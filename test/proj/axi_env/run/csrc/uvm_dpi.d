0fe0e45a3ee2987de914eefc68dd51d64fbf76ef
Charry Wang <18364998790@163.com>
2025-09-16 18:35:31 +0800

    clean up

--- test/proj/axi_env/run/csrc/uvm_dpi.d
+++
@@ -1,9 +0,0 @@
-uvm_dpi.o: /home/eda/synopsys/vcs/T-2022.06-SP2/etc/uvm/dpi/uvm_dpi.cc \
- /home/eda/synopsys/vcs/T-2022.06-SP2/etc/uvm/dpi/uvm_regex.cc \
- /home/eda/synopsys/vcs/T-2022.06-SP2/include/vpi_user.h \
- /home/eda/synopsys/vcs/T-2022.06-SP2/etc/uvm/dpi/uvm_hdl.c \
- /home/eda/synopsys/vcs/T-2022.06-SP2/include/sv_vpi_user.h \
- /home/eda/synopsys/vcs/T-2022.06-SP2/include/vpi_user.h \
- /home/eda/synopsys/vcs/T-2022.06-SP2/include/veriuser.h \
- /home/eda/synopsys/vcs/T-2022.06-SP2/include/svdpi.h \
- /home/eda/synopsys/vcs/T-2022.06-SP2/etc/uvm/dpi/uvm_svcmd_dpi.c
